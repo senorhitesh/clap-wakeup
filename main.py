20898db5b391dabb840f699306792635de5a874d
Hitesh Suthar <senorhitesh@gmail.com>
2026-07-16 10:34:54 +0530

    feat: improve sound recognition for accurate clap detection

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 import sounddevice as sd
-from scipy.io.wavfile import write
 from application_open import run_appication
 import librosa
+import numpy
 # parameters
-fs = 4000
-duration = 0.03
-channel = 1
+fs = 16000 
+threshold = 0.3
+block  = 512  # ~32ms blocks, fine for RMS
 
-while True:
-    audio = sd.rec(
-        int(fs* duration),
-        samplerate=fs,
-        channels=channel,
-        dtype="float32"
-    )
-    sd.wait()
-    pitch,_,_ = librosa.pyin(audio.flatten(), fmin=50, fmax=1500)
-    print(pitch)
 
-    if(pitch > 1000):
-        run_appication()
-        break
+def is_loud_transient(chunk, prev_rms, threshold):
+    rms = numpy.sqrt(numpy.mean(chunk**2))
+    jump =  rms - prev_rms
+    return jump > threshold, rms
 
+prev_rms = 0
+
+with sd.InputStream(samplerate=fs, channels=1, blocksize=block) as stream:
+    while True:
+        chunk,_ = stream.read(block)
+        print(chunk)
+        chunk = chunk.flatten()
+        print(chunk)
+        triggered, prev_rms = is_loud_transient(chunk,prev_rms, threshold)
+        print(triggered)
+        print(prev_rms)
+        if triggered:
+            run_appication()
+            break
